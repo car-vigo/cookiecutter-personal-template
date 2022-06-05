9844fec498df0c7462c42c0224f2713021888858
Carlos Vigo <59539262+car-vigo@users.noreply.github.com>
2022-06-05 10:50:18 -0300

    fix(setup.py): bad reference to project_author_name variable

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -20,8 +20,8 @@ def readme() -> str:
 setup(
     name="{{ cookiecutter.project_slug }}",
     version="{{ cookiecutter.project_version }}",
-    author="{{ cookiecutter.author_name }}",
-    author_email="{{ cookiecutter.author_email }}",
+    author="{{ cookiecutter.project_author_name }}",
+    author_email="{{ cookiecutter.project_author_email }}",
     description="{{ cookiecutter.project_description }}",
     python_requires=">=3",
     license="MIT",
